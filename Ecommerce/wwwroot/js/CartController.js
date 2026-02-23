d11a7d4619777478b10d50041c3543e1cfec7f9d
Md Zihadur Rahman Zihad <zihadurrahmman@gmail.com>
2026-02-23 23:42:31 +0600

    feat: Add cart open from sidebar.

--- Ecommerce/wwwroot/js/CartController.js
+++ Ecommerce/wwwroot/js/CartController.js
@@ -1,37 +1,53 @@
-﻿let CartController = {
+﻿$(document).ready(function () {
+    CartController.getProductsFromLocalStorage();
+    $.each(CartController.productList, function (index, product) {
+        CartController.addProductIntoDOM(product);
+    });
+    CartController.setCartProductCount();
+});
+let CartController = {
     productList : [],
     addToCart: function(productId) {
         $.get("https://dummyjson.com/products/" + productId, (response) => {
-            let existingCount = $('#cart-item-count').html();
-            $('#cart-item-count').html(parseInt(existingCount) + 1);
+            this.getProductsFromLocalStorage();
             this.productList.push(response);
+            this.setItemIntoLocalStorage();
+            this.addProductIntoDOM(response);
+            this.setCartProductCount();
+            this.showCart();
         });
     },
-    showProductList : function() {
-        console.log(this.productList);
-        $("#products-container").html('');
-        $.each(this.productList, function (index, product) {
-            $("#products-container").append(`
-            <div class="col col-12">
-                <div class="card mb-3" style="max-width: 540px;">
-                  <div class="row g-0">
-                    <div class="col-md-4">
-                      <img src="${product.images[0]}" style="height: 100px" class="img-fluid rounded-start" alt="...">
+    setCartProductCount: function () {
+        $('#cart-item-count').html(CartController.productList.length);
+    },
+    getProductsFromLocalStorage: function () {
+        if (localStorage.getItem("cartProducts") !== null) {
+            CartController.productList = JSON.parse(localStorage.getItem("cartProducts"));
+        }
+    },
+    setItemIntoLocalStorage: function () {
+        localStorage.setItem("cartProducts", JSON.stringify(this.productList));
+    },
+    addProductIntoDOM : function(product) {
+        $("#products-container").append(`
+        <div class="col col-12">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${product.images[0]}" style="height: 100px" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text"><small class="text-body-secondary">${product.price}</small></p>
                     </div>
-                    <div class="col-md-8">
-                      <div class="card-body">
-                        <h5 class="card-title">${product.title}</h5>
-                        <p class="card-text"><small class="text-body-secondary">${product.price}</small></p>
-                      </div>
-                    </div>
-                  </div>
+                </div>
                 </div>
             </div>
-        `);
-        });
+        </div>
+    `);
     },
     showCart: function () {
-        this.showProductList();
         $('#dv-cart').animate({
             right: '0px'
         }, 400);
